3ae0f4aa14848e6f7dfe233d80a81f25bcd00f1a
pdelebarre <pdelebarre@gmail.com>
2021-03-08 16:57:15 +0100

    Initial commit

---
+++ src/main/java/edu/delebarre/appindocker/AppindockerApplication.java
@@ -0,0 +1,13 @@
+package edu.delebarre.appindocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppindockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppindockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/delebarre/appindocker/AppindockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.delebarre.appindocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppindockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
